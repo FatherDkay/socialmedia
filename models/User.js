e781b5b7b7e6f9e781af91843b46791055abf913
FatherDkay <paul.d.bailey@hotmail.com>
2022-01-02 16:51:19 -0800

    user done routes tested

---
+++ models/User.js
@@ -0,0 +1,50 @@
+const { Schema, model } = require('mongoose');
+
+//function to validate email
+var validateEmail = function(email) {
+    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email)
+};
+
+const UserSchema = new Schema ({
+    username: {
+        type: String,
+        unique: true,
+        required: 'Username is required',
+        trim: true
+    },
+    email: {
+        type: String,
+        trim: true,
+        required: 'Email is required',
+        unique: true,
+        validate: [validateEmail, 'Please fill a valid email address'],
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+    },
+    thoughts: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: 'Thought'
+        }
+      ],
+    friends: []
+    },
+    {
+        toJSON: {
+            virtual: true,
+            getters: true
+        },
+    }
+);
+
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+
+UserSchema.virtual('thoughtCount').get(function() {
+    return this.thoughts.length;
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
